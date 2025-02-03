c259b4d96e243320a6aad227dd0db46e7c2dffb6
Nihel Ben Mahfoudh <nihel.belhadj@gmail.com>
2025-02-03 11:56:55 +0100

    Add entity User

---
+++ src/Entity/User.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ORM\HasLifecycleCallbacks]//gestion de creation et modification
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    #[ORM\Column(length: 80, nullable: true)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 80, nullable: true)]
+    private ?string $fullname = null;
+
+    #[ORM\Column]
+    private ?bool $is_minor = null;
+
+    #[ORM\Column]
+    private ?bool $is_terms = null;
+
+    #[ORM\Column]
+    private ?bool $is_gpdr = null;
+
+      // Constructeur pour gérer les attributs non-nullables par défault
+      public function __construct()
+      {
+          $this->is_minor = false;
+          $this->is_terms = false;
+          $this->is_gpdr = false;
+      }
+      #[ORM\PrePersist]
+      public function setCreatedValues(): void
+      {
+          $this->created_at = new \DateTimeImmutable();
+      } 
+      #[ORM\PreUpdate]
+      public function updateTimestamps(): void
+      {
+          $this->updated_at = new \DateTimeImmutable();
+      }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+  
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getFullname(): ?string
+    {
+        return $this->fullname;
+    }
+
+    public function setFullname(?string $fullname): static
+    {
+        $this->fullname = $fullname;
+
+        return $this;
+    }
+
+    public function isMinor(): ?bool
+    {
+        return $this->is_minor;
+    }
+
+    public function setIsMinor(bool $is_minor): static
+    {
+        $this->is_minor = $is_minor;
+
+        return $this;
+    }
+
+    public function isTerms(): ?bool
+    {
+        return $this->is_terms;
+    }
+
+    public function setIsTerms(bool $is_terms): static
+    {
+        $this->is_terms = $is_terms;
+
+        return $this;
+    }
+
+    public function isGpdr(): ?bool
+    {
+        return $this->is_gpdr;
+    }
+
+    public function setIsGpdr(bool $is_gpdr): static
+    {
+        $this->is_gpdr = $is_gpdr;
+
+        return $this;
+    }
+}
